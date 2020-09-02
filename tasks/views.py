62e8e83b55459934eb0463c8c6746c3dc077b2fe
raj <rajd810@outlook.com>
2020-09-02 21:47:25 +0530

    ToDo

---
+++ tasks/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse		
+
+from .models import *
+from .forms import *
+
+#Views for task creation
+def index(request):
+	tasks = Task.objects.all()
+
+	form = TaskForm()
+
+#Need to create post method with submit
+	if request.method == 'POST':
+		form = TaskForm(request.POST)
+		if form.is_valid():
+			form.save()
+		return redirect('/')
+
+	context = {'tasks': tasks, 'form': form}
+	return render(request, "tasks/index.html", context)
+
+
+#Views for task updation
+def updateTask(request, pk):
+	task = Task.objects.get(id=pk)
+
+	form = TaskForm(instance = task)
+
+	if request.method == 'POST':
+		form = TaskForm(request.POST, instance=task)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context = {'form': form}
+
+	return render(request, 'tasks/update_task.html', context)
+
+
+#Views for task deletion
+def deleteTask(request, pk):
+	item = Task.objects.get(id= pk)
+
+	if request.method == 'POST':
+		item.delete()
+		return redirect('/')
+
+	context = {'item': item}
+
+	return render(request, 'tasks/delete.html', context)
